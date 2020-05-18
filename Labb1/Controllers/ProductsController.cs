0e50ce4495e7aa145bee3e5cc9b3d8053d6f2388
vicbac300 <43798306+vicbac300@users.noreply.github.com>
2020-05-18 13:45:25 +0200

    .

---
+++ Labb1/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labb1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Labb1/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labb1.Models
+{
+	public class Product : A
+	{
+		private int ID { get; set; } = 10;
+		public object CacheID { get { return ID; } }
+	}
+
+	interface A
+	{
+		object CacheID { get; }
+	}
+}
