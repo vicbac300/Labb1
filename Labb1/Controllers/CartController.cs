e27327ab552772e8e383a7794a2dbfeff8559073
vicbac300 <43798306+vicbac300@users.noreply.github.com>
2020-05-21 11:50:25 +0200

    .

---
+++ Labb1/Controllers/CartController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Labb1.Services;
+using Labb1.ViewModels;
+using Labb1.Models;
+
+namespace Labb1.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService productService;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public CartController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+            this.productService = new MockProductService();
+        }
+
+        public IActionResult Index()
+        {
+            var cart = Request.Cookies.SingleOrDefault(cookie => cookie.Key == "cart");
+            var cartIds = cart.Value.Split(',');
+            var products = productService.GetAll();
+
+            CartViewModel vm = new CartViewModel();
+            vm.CreatedTicks = DateTime.Now.Ticks;
+            vm.Products = new Dictionary<int, CartItem>();
+
+            foreach (string idStr in cartIds)
+            {
+                int id = int.Parse(idStr);
+
+                if (vm.Products.ContainsKey(id))
+                    vm.Products[id].Amount++;
+                else
+                {
+                    vm.Products.Add(id,
+                        new CartItem() { Product = productService.GetByID(id), Amount = 1 });
+                }
+                    
+
+
+            }
+
+            // Totalpris.
+            decimal total = 0m;
+            foreach (var item in vm.Products.Values)
+            {
+                total += (item.Product.Price * item.Amount);
+            }
+            vm.TotalPrice = total;
+            ///
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult PlaceOrder([Bind("TotalPrice,Products")]CartViewModel vm)
+        {
+            Order order = new Order();
+            order.TotalPrice = vm.TotalPrice;
+            order.Date = DateTime.Now;
+            order.OrderRows = vm.Products.Values.ToOrderRowList();
+            order.UserID = int.Parse(userManager.GetUserId(User));
+
+            return RedirectToAction("OrderSuccess", order);
+        }
+
+        public IActionResult OrderSuccess(Order order)
+        {
+
+            return View(order);
+        }
+    }
+}
--- Labb1/Controllers/HomeController.cs
+++ Labb1/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace Labb1.Controllers
 			return View();
 		}
 
-		public IActionResult Products()
-		{
-			return View();
-		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
